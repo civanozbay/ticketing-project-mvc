95fa9d529b3242f1e7af18a4c2c8396f66f2f072
civanozbay <civan417@hotmail.com>
2022-10-01 21:14:44 +0200

    update

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -7,6 +7,8 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,8 +27,19 @@ public class UserController {
     public String createUser(Model model){
         model.addAttribute("users",userService.findAll());
         model.addAttribute("roles",roleService.findAll());
-        model.addAttribute("user",new User());
+        model.addAttribute("user",new UserDTO());
         return "user/create";
+    }
+    @PostMapping("/create")
+    public String insertUser(@ModelAttribute("user")UserDTO user, Model model){
+
+        model.addAttribute("user",new UserDTO());
+        model.addAttribute("roles",roleService.findAll());
+
+        userService.save(user);
+        model.addAttribute("users",userService.findAll());
+
+        return "/user/create"; //user,roles,users (you should handle these objects if you want to navigate user the same page.
     }
 
 }
