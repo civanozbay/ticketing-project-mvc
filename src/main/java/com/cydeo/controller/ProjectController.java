ddbd4829d96d042f5b9bbffeba46c3bcf79007c0
civanozbay <civan417@hotmail.com>
2022-10-04 18:47:34 +0200

    showing the project infos

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -6,6 +6,8 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -29,5 +31,13 @@ public class ProjectController {
 
 
         return "/project/create";
+    }
+
+    @PostMapping("/create")
+    public String addProject(@ModelAttribute("project") ProjectDTO projectDTO,Model model){
+
+        projectService.save(projectDTO);
+
+        return "redirect:/project/create";
     }
 }
